b09dd7720226479cedc4c82814179135a0f0dac1
Amy <amam6422@gmail.com>
2023-07-07 16:19:44 +0800

    finish testing not index file and remove the import

--- apps/telu-app/src/index.ts
+++ apps/telu-app/src/index.ts
@@ -1,9 +1,7 @@
 import TestSrcPath from './testPath'
-import CallCenter2 from '@heimdallr/services/CallCenter2'
 import CallCenter from '@heimdallr/services/CallCenter'
 
 import App from './App'
 // eslint-disable-next-line no-console
 console.log(TestSrcPath)
-console.log(CallCenter2)
 console.log(CallCenter)
