fa924d8bff6df56b72ca15a6d927e56fafe7c899
Amy <amam6422@gmail.com>
2023-07-07 15:59:28 +0800

    import CallCenter2

--- apps/telu-app/src/index.ts
+++ apps/telu-app/src/index.ts
@@ -1,7 +1,9 @@
 import TestSrcPath from './testPath'
+import CallCenter2 from '@heimdallr/services/CallCenter2'
 import CallCenter from '@heimdallr/services/CallCenter'
 
 import App from './App'
 // eslint-disable-next-line no-console
 console.log(TestSrcPath)
+console.log(CallCenter2)
 console.log(CallCenter)
