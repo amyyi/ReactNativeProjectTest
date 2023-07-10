2a11ac9c541ae24cac64e3130292d0fd341459f6
Amy <amam6422@gmail.com>
2023-07-10 17:30:12 +0800

    remove import

--- apps/telu-app/src/index.ts
+++ apps/telu-app/src/index.ts
@@ -1,7 +1,5 @@
-import TestSrcPath from '@heimdallr/src/testPath'
 import CallCenter from '@heimdallr/services/CallCenter'
 
 import App from './App'
 // eslint-disable-next-line no-console
-console.log(TestSrcPath)
 console.log(CallCenter)
